1a23a1f3128e51db377780076d87ff3a42acc053
Ande5 <33572132+Ande5@users.noreply.github.com>
2017-12-16 19:45:03 +0300

    Revert "Обновление SystemLogic"
    This reverts commit 5d5c06938fa39ed24516c207b90543b64ad43d2a.

--- IntelligenceSystem/RollingDiagramma.cs
+++ IntelligenceSystem/RollingDiagramma.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
-using SystemLogic;
+
 namespace IntelligenceSystem
 {
     public partial class RollingDiagramma : Form
